4fde05284edddb7de7a79a7fe6c670cf559eaa84
Son Sumin <clvswft03@gmail.com>
2024-06-25 18:35:47 +0900

    style(format): format with 'nix fmt'

--- home/nixvim/plugins/hop.nix
+++ home/nixvim/plugins/hop.nix
@@ -3,58 +3,58 @@
     plugins.hop = {
       enable = true;
 
-        # keymaps = [
-        #   {
-        #     key = "f";
-        #     action.__raw = \'\'
-        #       function()
-        #         require'hop'.hint_char1({
-        #           direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
-        #           current_line_only = true
-        #         })
-        #       end
-        #     \'\';
-        #     options.remap = true;
-        #   }
-        #   {
-        #     key = "F";
-        #     action.__raw = \'\'
-        #       function()
-        #         require'hop'.hint_char1({
-        #           direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
-        #           current_line_only = true
-        #         })
-        #       end
-        #     \'\';
-        #     options.remap = true;
-        #   }
-        #   {
-        #     key = "t";
-        #     action.__raw = \'\'
-        #       function()
-        #         require'hop'.hint_char1({
-        #           direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
-        #           current_line_only = true,
-        #           hint_offset = -1
-        #         })
-        #       end
-        #     \'\';
-        #     options.remap = true;
-        #   }
-        #   {
-        #     key = "T";
-        #     action.__raw = \'\'
-        #       function()
-        #         require'hop'.hint_char1({
-        #           direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
-        #           current_line_only = true,
-        #           hint_offset = 1
-        #         })
-        #       end
-        #     \'\';
-        #     options.remap = true;
-        #   }
-        # ];
+      # keymaps = [
+      #   {
+      #     key = "f";
+      #     action.__raw = \'\'
+      #       function()
+      #         require'hop'.hint_char1({
+      #           direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+      #           current_line_only = true
+      #         })
+      #       end
+      #     \'\';
+      #     options.remap = true;
+      #   }
+      #   {
+      #     key = "F";
+      #     action.__raw = \'\'
+      #       function()
+      #         require'hop'.hint_char1({
+      #           direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+      #           current_line_only = true
+      #         })
+      #       end
+      #     \'\';
+      #     options.remap = true;
+      #   }
+      #   {
+      #     key = "t";
+      #     action.__raw = \'\'
+      #       function()
+      #         require'hop'.hint_char1({
+      #           direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+      #           current_line_only = true,
+      #           hint_offset = -1
+      #         })
+      #       end
+      #     \'\';
+      #     options.remap = true;
+      #   }
+      #   {
+      #     key = "T";
+      #     action.__raw = \'\'
+      #       function()
+      #         require'hop'.hint_char1({
+      #           direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+      #           current_line_only = true,
+      #           hint_offset = 1
+      #         })
+      #       end
+      #     \'\';
+      #     options.remap = true;
+      #   }
+      # ];
 
     };
   };
