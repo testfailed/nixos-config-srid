e905b2ea804e94ec873c159b5bedbd5785761c54
Son Sumin <clvswft03@gmail.com>
2024-10-29 13:43:37 +0900

    chore: minor changes

--- flake.nix
+++ flake.nix
@@ -6,16 +6,16 @@
 
   nixConfig = {
     substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-      "https://cachix.cachix.org"
-      "https://devenv.cachix.org"
+      "https://cache.nixos.org?priority=10"
+      "https://nix-community.cachix.org?priority=20"
+      "https://cachix.cachix.org?priority=30"
+      "https://devenv.cachix.org?priority=30"
       # "https://nammayatri.cachix.org?priority=42"
       # "https://cache.garnix.io?priority=41"
     ];
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       # "nammayatri.cachix.org-1:PiVlgB8hKyYwVtCAGpzTh2z9RsFPhIES6UKs0YB662I="
